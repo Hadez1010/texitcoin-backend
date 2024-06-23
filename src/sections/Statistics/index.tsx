4fde4d50d8a00e0095a0a2a076092e96238bb45b
vitalii <vitovodenko@gmail.com>
2024-06-23 15:13:30 -0400

    Integrated backend apis Statistics

--- src/sections/Statistics/index.tsx
+++ src/sections/Statistics/index.tsx
@@ -1,13 +1,86 @@
+import { useMemo } from 'react';
+import { useQuery as useGraphQuery } from '@apollo/client';
+
 import Grid from '@mui/material/Unstable_Grid2';
 import Container from '@mui/material/Container';
 import { useTheme } from '@mui/material/styles';
+
+import { useQuery } from 'src/routes/hooks';
 
 import ChartWidget from 'src/components/ChartWidget';
 import WidgetSummary from 'src/components/WidgetSummary';
 import CollapsibleTable from 'src/components/CollapsibleTable';
 
+import { IStatisticsFilters, IStatisticsPrismaFilter } from 'src/types/statistics';
+
+import {
+  FETCH_BLOCKS_QUERY,
+  FETCH_MEMBERS_COUNT,
+  FETCH_MEMBERS_BY_DATE,
+  FETCH_STATISTICS_QUERY,
+} from './query';
+
+const defaultFilter: IStatisticsFilters = {
+  search: '',
+  newBlocks: 0,
+  totalBlocks: 0,
+  newHashPower: 0,
+  totalHashPower: 0,
+  members: 0,
+};
+
 export default function StatisticsSection() {
   const theme = useTheme();
+
+  const [query] = useQuery<IStatisticsFilters>();
+
+  const { filter = defaultFilter } = query;
+
+  const graphQueryFilter = useMemo(() => {
+    const filterObj: IStatisticsPrismaFilter = {};
+
+    if (filter.search) {
+      filterObj.OR = [{}];
+    }
+
+    return filterObj;
+  }, [filter]);
+
+  const { loading: statisticsLoading, data: statisticsData } = useGraphQuery(
+    FETCH_STATISTICS_QUERY,
+    {
+      variables: {
+        page: '1,1000',
+        filter: graphQueryFilter,
+        sort: '-createdAt',
+      },
+    }
+  );
+
+  const { loading: blockLoading, data: blocksData } = useGraphQuery(FETCH_BLOCKS_QUERY, {
+    variables: {
+      page: '1,1000',
+      filter: graphQueryFilter,
+      sort: 'blockNo',
+    },
+  });
+
+  const { loading: memberLoading, data: members } = useGraphQuery(FETCH_MEMBERS_COUNT);
+
+  const { loading: rateLoading, data: membersRate } = useGraphQuery(FETCH_MEMBERS_BY_DATE);
+
+  const today = new Date().toLocaleDateString('en-CA').split('T')[0];
+
+  const statistics = statisticsLoading ? { statistics: [], total: 0 } : statisticsData?.statistics;
+  const blocks = blockLoading ? { blocks: [], total: 0 } : blocksData?.blocks;
+  const membersGroupByDate = rateLoading
+    ? { rates: [], total: 0 }
+    : membersRate?.membersGroupByDate;
+
+  const newBlocks = blocks!.blocks!.reduce(
+    (prev, item) => (item?.createdAt > today ? prev + 1 : prev),
+    0
+  );
 
   return (
     <Container maxWidth="xl">
@@ -16,10 +89,10 @@ export default function StatisticsSection() {
           <WidgetSummary
             title="Total Blocks"
             percent={2.6}
-            total={15564}
+            total={blocks!.total!}
             chart={{
               colors: [theme.palette.primary.light, theme.palette.primary.main],
-              series: [22, 8, 35, 50, 82, 84, 77, 12, 87, 43],
+              series: statistics!.statistics!.map((item) => item!.totalBlocks),
             }}
           />
         </Grid>
@@ -27,10 +100,10 @@ export default function StatisticsSection() {
           <WidgetSummary
             title="New Blocks"
             percent={-0.1}
-            total={384}
+            total={newBlocks}
             chart={{
               colors: [theme.palette.info.light, theme.palette.info.main],
-              series: [56, 47, 40, 62, 73, 30, 23, 54, 67, 68],
+              series: statistics!.statistics!.map((item) => item!.newBlocks),
             }}
           />
         </Grid>
@@ -38,10 +111,10 @@ export default function StatisticsSection() {
           <WidgetSummary
             title="Members"
             percent={0.6}
-            total={102}
+            total={memberLoading ? 0 : members!.members!.total!}
             chart={{
               colors: [theme.palette.secondary.light, theme.palette.secondary.main],
-              series: [40, 70, 75, 70, 50, 28, 7, 64, 38, 27],
+              series: membersGroupByDate!.rates!.map((item) => item!.count),
             }}
           />
         </Grid>
@@ -49,66 +122,54 @@ export default function StatisticsSection() {
       <Grid container spacing={3}>
         <Grid xs={12} md={6}>
           <ChartWidget
+            key="hashRate"
             title="Hashrate"
-            subheader="(+20%) than last day"
             chart={{
-              categories: [
-                '38312',
-                '38313',
-                '38314',
-                '38315',
-                '38316',
-                '38317',
-                '38318',
-                '38319',
-                '38320',
-                '38321',
-                '38322',
-                '38323',
-              ],
+              categories: blocks!.blocks!.map((item) => item?.blockNo!),
               series: [
                 {
                   data: [
                     {
                       name: 'Hashrate',
-                      data: [100, 410, 350, 510, 490, 620, 690, 910, 148, 350, 510, 490],
+                      data: blocks!.blocks!.map((item) =>
+                        Math.floor(1000 * (item?.hashRate! || 1))
+                      ),
                     },
                   ],
                 },
               ],
+              options: {
+                xaxis: {
+                  tickAmount: 20,
+                },
+              },
             }}
           />
         </Grid>
         <Grid xs={12} md={6}>
           <ChartWidget
-            title="Pos Difficulty"
-            subheader="(+20%) than last day"
+            key="difficulty"
+            title="Network Difficulty"
             chart={{
               colors: ['#ffb136'],
-              categories: [
-                '38312',
-                '38313',
-                '38314',
-                '38315',
-                '38316',
-                '38317',
-                '38318',
-                '38319',
-                '38320',
-                '38321',
-                '38322',
-                '38323',
-              ],
+              categories: blocks!.blocks!.map((item) => item?.blockNo!),
               series: [
                 {
                   data: [
                     {
                       name: 'Pos Difficulty',
-                      data: [100, 340, 130, 560, 770, 440, 990, 770, 450, 130, 560, 770],
+                      data: blocks!.blocks!.map((item) =>
+                        Math.floor(1000 * (item?.difficulty! || 1))
+                      ),
                     },
                   ],
                 },
               ],
+              options: {
+                xaxis: {
+                  tickAmount: 20,
+                },
+              },
             }}
           />
         </Grid>
